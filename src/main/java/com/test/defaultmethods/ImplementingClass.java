f9b4fd20f02dba67f3d38d281ee61096cb7866d1
matijavizintin <matija.vizintin@gmail.com>
2015-07-31 14:59:21 +0200

    synchronization test
    cleanup

--- src/main/java/com/test/defaultmethods/ImplementingClass.java
+++
@@ -1,9 +0,0 @@
-package com.test.defaultmethods;
-
-/**
- * Created by Matija Vižintin
- * Date: 08. 05. 2015
- * Time: 14.12
- */
-public class ImplementingClass implements DefaultInterface {
-}
--- src/test/java/com/test/concurrency/SynchronizationTest.java
+++ src/test/java/com/test/concurrency/SynchronizationTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -135,7 +136,6 @@ public class SynchronizationTest extends LoggingTimedTest {
      * Optimistic locking example. Optimistic lock is a non-blocking lock so doesn't prevent write lock to be acquired. When a write lock is acquired
      * optimistic lock is no longer valid. Note than even when write lock is released optimistic lock is still invalid.
      *
-     *
      * @throws InterruptedException
      */
     @Test(timeout = 2150)
@@ -145,47 +145,139 @@ public class SynchronizationTest extends LoggingTimedTest {
         // optimistic locking
         StampedLock lock = new StampedLock();
         service.submit(() -> {
-                          long stamp = lock.tryOptimisticRead();
-                           System.out.printf("%s Trying optimistic read lock\n", Clock.systemUTC().instant());
-                          try {
-                              System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
-                              TimeUnit.SECONDS.sleep(1);
-                              System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
-                              TimeUnit.SECONDS.sleep(1);
-                              System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
-                          } catch (Exception e) {
-                              // pass
-                          } finally {
-                              lock.unlock(stamp);
-                              System.out.printf("%s Optimistic lock released\n", Clock.systemUTC().instant());
-                          }
-                       });
+            long stamp = lock.tryOptimisticRead();
+            System.out.printf("%s Trying optimistic read lock\n", Clock.systemUTC().instant());
+            try {
+                System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
+                TimeUnit.SECONDS.sleep(1);
+                System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
+                TimeUnit.SECONDS.sleep(1);
+                System.out.printf("%s Read lock acquired: %b\n", Clock.systemUTC().instant(), lock.validate(stamp));
+            } catch (Exception e) {
+                // pass
+            } finally {
+                lock.unlock(stamp);
+                System.out.printf("%s Optimistic lock released\n", Clock.systemUTC().instant());
+            }
+        });
 
         // write lock
         service.submit(() -> {
-                          long stamp = lock.writeLock();
-                           System.out.printf("%s Write lock acquired\n", Clock.systemUTC().instant());
-                          try {
-                              TimeUnit.SECONDS.sleep(1);
-                          } catch (Exception e) {
-                              // pass
-                          } finally {
-                              lock.unlock(stamp);
-                              System.out.printf("%s Write lock released\n", Clock.systemUTC().instant());
-                          }
-                       });
+            long stamp = lock.writeLock();
+            System.out.printf("%s Write lock acquired\n", Clock.systemUTC().instant());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                // pass
+            } finally {
+                lock.unlock(stamp);
+                System.out.printf("%s Write lock released\n", Clock.systemUTC().instant());
+            }
+        });
 
         // shutdown
         service.awaitTermination(2050, TimeUnit.MILLISECONDS);
     }
 
-    @Test
+    /**
+     * This example show how to convert from read to write lock without blocking. If conversion fails we can still decide
+     * to block the thread.
+     *
+     * @throws InterruptedException
+     */
+    @Test(timeout = 1300)
     public void convertReadToWriteLock() throws InterruptedException {
         ExecutorService service = Executors.newScheduledThreadPool(ForkJoinTest.NO_OF_VCORES);
 
         // optimistic locking
         StampedLock lock = new StampedLock();
+        service.submit(() -> {
+            long stamp = lock.tryOptimisticRead();
 
-        // TODO: ...
+            try {
+                TimeUnit.MILLISECONDS.sleep(20);
+                System.out.printf("%s Trying to convert to write lock.\n", Clock.systemUTC().instant());
+                stamp = lock.tryConvertToWriteLock(stamp);
+                if (stamp == 0) {
+                    System.out.printf("%s Couldn't convert to write lock.\n", Clock.systemUTC().instant());
+                    stamp = lock.writeLock();
+                    System.out.printf("%s Write lock forced for stamp %d.\n", Clock.systemUTC().instant(), stamp);
+                } else {
+                    System.out.printf("%s Converted to write lock for stamp %d.\n", Clock.systemUTC().instant(), stamp);
+                }
+            } catch (InterruptedException e) {
+                // pass
+            } finally {
+                lock.unlock(stamp);
+                System.out.printf("%s Write lock released for stamp %d.\n", Clock.systemUTC().instant(), stamp);
+            }
+        });
+
+        // write lock to prevent read to write lock conversion
+        service.submit(() -> {
+            long stamp = 0;
+            try {
+                System.out.printf("%s Acquiring write lock.\n", Clock.systemUTC().instant());
+                stamp = lock.writeLock();
+                System.out.printf("%s Write lock acquired for stamp %d.\n", Clock.systemUTC().instant(), stamp);
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                // pass
+            } finally {
+                lock.unlock(stamp);
+                System.out.printf("%s Write lock released for stamp %d.\n", Clock.systemUTC().instant(), stamp);
+            }
+        });
+
+        // shutdown
+        service.awaitTermination(1200, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * This is a semaphore test that shows how to use semaphores. They don't limit you to exclusive locks, we can allow
+     * multiple concurrent access to the resource, but only to a limited number of resources.
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void semaphores() throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(5);
+
+        // create a semaphore that allows 2 concurrent accesses to the resource
+        Semaphore semaphore = new Semaphore(2);
+
+        // create task
+        Runnable task = () -> {
+            // try acquiring access
+            try {
+                boolean acquired = semaphore.tryAcquire(100, TimeUnit.MICROSECONDS);
+
+                if (acquired) {
+                    // lock acquired, sleep (fake work) for 500 ms
+                    System.out.printf(
+                            "%s Semaphore access acquired by thread %s. Sleeping for 2 seconds.\n", Clock.systemUTC().instant(),
+                            Thread.currentThread().getName());
+
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(500);
+                    } finally {
+                        semaphore.release();
+                        System.out.printf("%s Semaphore released by thread %s\n", Clock.systemUTC().instant(), Thread.currentThread().getName());
+                    }
+                } else {
+                    System.out.printf(
+                            "%s Couldn't acquire semaphore access. Thread: %s\n", Clock.systemUTC().instant(), Thread.currentThread().getName());
+                }
+            } catch (InterruptedException e) {
+                System.out.printf("%s Failed while acquiring semaphore access.\n", Clock.systemUTC().instant());
+            }
+
+        };
+
+        // execute 5 tasks (we have a pool of 5 threads)
+        IntStream.range(0, 5).forEach(value -> service.submit(task));
+
+        // shutdown
+        service.awaitTermination(700, TimeUnit.MILLISECONDS);
     }
 }
