7f3d28cc6d28a9522cd2b5f7c0597bd2bd483473
matijavizintin <matija.vizintin@gmail.com>
2015-08-28 19:44:33 +0200

    tests todo

---
+++ src/test/java/com/test/concurrency/AtomicVariablesTest.java
@@ -0,0 +1,12 @@
+package com.test.concurrency;
+
+import com.test.timed.LoggingTimedTest;
+
+/**
+ * Created by Matija Vižintin
+ * Date: 28. 08. 2015
+ * Time: 18.50
+ */
+public class AtomicVariablesTest extends LoggingTimedTest {
+    // TODO
+}
